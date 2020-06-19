e253bf2d2bf2906a669d4e3c5bba5a5aea516657
LuisRivero021298 <luisda021298@gmail.com>
2020-06-19 01:54:31 -0500

    Icons, btn-add-list

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,33 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
-import { UserModel } from '../../models/user.model';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+import { UserModel } from "../../models/user.model";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styles: []
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styles: [],
 })
 export class HomeComponent implements OnInit {
-	profile: UserModel;
+  profile: UserModel;
 
-  constructor( private _auth: AuthService ) {
-  	this.profile = new UserModel({});
+  constructor(private _auth: AuthService) {
+    this.profile = new UserModel({
+      username: "patricia123",
+      email: "patricia123@gmail.com",
+      photo: "node_js_hexagon-wallpaper-1920x1200.jpg",
+      birthdate: "1997-06-04T05:00:00.000Z",
+    });
   }
 
   ngOnInit(): void {
-  	this.getProfile();
+    this.getProfile();
   }
 
   getProfile() {
-  	const token = this._auth.readToken();
-  	this._auth.getUser(token).subscribe(
-  		resp => {
-  			this.profile = resp;
-  		},
-  		err => {
-  			console.log(err);
-  		}
-  	)
+    const token = this._auth.readToken();
+    this._auth.getUser(token).subscribe(
+      (resp) => {
+        this.profile = resp;
+        console.log(resp);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
-
 }
