470330fb6af2577feb8622eea5f8084073a070b3
LuisRivero021298 <luisda021298@gmail.com>
2020-06-21 13:27:58 -0500

    Responsive pages (ipad)

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ListComponent implements OnInit {
+    screenSize = {
+        screenWidth: 0,
+        screenHeight: 0
+    }
 
   constructor() { }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
+        this.screenSize.screenWidth = screen.height;
+        this.screenSize.screenWidth = screen.width;
   }
 
 }
